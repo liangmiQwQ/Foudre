fde9c184827736b0e6a550c12b9f6866bd4fa225
liangmiQwQ <mi_yyds@126.com>
2024-09-18 21:05:13 +0800

    feat: init NavBar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import { StyledEngineProvider } from "@mui/material/styles";
+import NavBar from "./NavBar";
 
 function App() {
-  return <StyledEngineProvider injectFirst></StyledEngineProvider>;
+  return (
+    <StyledEngineProvider injectFirst>
+      <NavBar></NavBar>
+    </StyledEngineProvider>
+  );
 }
 
 export default App;
